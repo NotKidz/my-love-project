480f23833830319bedb0b9fb5b1f67daee8b8387
NotKidz <nck.syhrl19@gmail.com>
2024-07-06 15:05:14 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// ... existing code ...
+
+// Get the love button
+const loveButton = document.querySelector('.lovein');
+
+// Add click event listener to the love button
+loveButton.addEventListener('click', (lovein) => {
+    audio.play(your_love_song.mp);
+});
+
+// ... rest of your code ...
